218ebf106f51adb0bfeb9f1c43d5ce1313b2b1d9
ThomasGruber24 <thomaspgruber@yahoo.com>
2025-11-15 11:42:14 -0700

    Forgot to transfer over Velocity.cpp/.h

--- 230.09.Ponder/velocity.h
+++ 230.09.Ponder/velocity.h
@@ -2,58 +2,53 @@
  * Header File:
  *    VELOCITY
  * Author:
- *    <your name here>
+ *    Br. Helfrich
  * Summary:
  *    Everything we need to know about speed
  ************************************************************************/
 
 #pragma once
 
-// for unit tests
+ // for unit tests
 class TestPosition;
 class TestVelocity;
 class TestAcceleration;
-class TestProjectile;
+class TestLander;
 
 // for add()
 class Acceleration;
 class Angle;
 
- /*********************************************
-  * Velocity
-  * I feel the need, the need for speed
-  *********************************************/
+/*********************************************
+ * Velocity
+ * I feel the need, the need for speed
+ *********************************************/
 class Velocity
 {
-   // for unit tests
-   friend TestPosition;
-   friend TestVelocity;
-   friend TestProjectile;
+	// for unit tests
+	friend TestPosition;
+	friend TestVelocity;
+	friend TestLander;
 
-   
 public:
-   // constructors
-   Velocity()                     : dx(9.9), dy(9.9) { }
-   Velocity(double dx, double dy) : dx(9.9), dy(9.9) { }
+	// constructors
+	Velocity();
+	Velocity(double dx, double dy);
 
-   // getters
-   double getDX()       const { return 9.9; }
-   double getDY()       const { return 9.9; }
-   double getSpeed()    const;
-   Angle  getAngle()    const;
-   
-   // setters
-   void set(const Angle & angle, double magnitude);
-   void setDX(double dx) {  }
-   void setDY(double dy) {  }
-   void addDX(double dx) {  }
-   void addDY(double dy) {  }
-   void add(const Acceleration & acceleration, double time);
-   void add(const Velocity & rhs) { }
-   void reverse() { }
+	// getters
+	double getDX() const;
+	double getDY() const;
+	double getSpeed() const;
+
+	// setters
+	void setDX(double dx);
+	void setDY(double dy);
+	void set(const Angle& angle, double magnitude);
+	void addDX(double dx);
+	void addDY(double dy);
+	void add(const Acceleration& acceleration, double time);
 
 private:
-   double dx;           // horizontal velocity
-   double dy;           // vertical velocity
+	double dx;           // horizontal velocity
+	double dy;           // vertical velocity
 };
-
